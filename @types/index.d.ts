8b1a2aad16b008e68653bf3030ad822055fa98b5
soal <soal@soal.red>
2023-07-31 17:13:09 +0300

    Code cleanup

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,19 +1,3 @@
-// export declare module "uv-list" {
-//   type UVListItem = {
-//     id: string | number;
-//     content?: string;
-//   };
-
-//   type UVListView = {
-//     uid: number;
-//     item: UVListItem;
-//   };
-
-//   interface IUvList extends HTMLElement {
-//     items: any[];
-//     updateVisibleItems: () => void;
-//   }
-// }
 export type UVListItem = {
   id: string | number;
   content?: string;
